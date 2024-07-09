026f42f3a08638dc9072e917b2cc0584f71f83b6
sree858585 <srikanthgara116@gmail.com>
2024-07-09 00:13:25 -0400

    Add files via upload

---
+++ Models/AgentClassificationData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace WebApplication1.Models
+{
+    public class AgentClassificationData
+    {
+        public int Id { get; set; }
+        public int Category { get; set; }
+        public bool Other { get; set; }
+        public int EaspSepsRegistrationID { get; set; }
+        public string OtherClassificationText { get; set; }  
+
+
+        public EaspSepsRegistration EaspSepsRegistration { get; set; }
+    }
+
+}
+
---
+++ Models/LkAgencyClassification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace WebApplication1.Models
+{
+    public class LkAgencyClassification
+    {
+        public int agency_classification_id { get; set; }
+        public string classifcation_description { get; set; }
+        public int sort_order { get; set; }
+        public bool is_active { get; set; }
+    }
+
+
+
+}
+
