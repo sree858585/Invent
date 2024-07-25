7bced54eaf61fbf0156e354ebda31f0b824997ea
sree858585 <srikanthgara116@gmail.com>
2024-07-24 22:18:33 -0400

    Add files via upload

--- Models/AgencyRegistration.cs
+++ Models/AgencyRegistration.cs
@@ -12,7 +12,7 @@ namespace WebApplication1.Models
         public string AlternateName { get; set; }
         [Required]
         public string County { get; set; }
-         [Required]
+        [Required]
         public string Address { get; set; }
         public string Address2 { get; set; }
         [Required]
@@ -25,7 +25,7 @@ namespace WebApplication1.Models
         public string UserId { get; set; }
         public List<EaspSepsRegistrationCounty> CountiesServed { get; set; } = new List<EaspSepsRegistrationCounty>();
         public ICollection<ShipToSite> ShipToSites { get; set; }
-        public ICollection<AgentClassificationData> AgentClassificationData { get; set; }
+        public ICollection<LnkAgencyClassificationData> LnkAgencyClassificationData { get; set; } // Updated property name
 
         public DateTime SubmissionDate { get; set; } // New property for submission date
 
