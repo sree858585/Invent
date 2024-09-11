4dadcb171c79bbf931860e8cbb97d716b9066c03
sree858585 <srikanthgara116@gmail.com>
2024-09-10 20:13:58 -0400

    Add files via upload

---
+++ Models/SmtpSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace WebApplication1.Models
+{
+    public class SmtpSettings
+    {
+        public bool IsDevelopment { get; set; }
+        public SmtpConfig DevSettings { get; set; }
+        public SmtpConfig ProdSettings { get; set; }
+    }
+
+    public class SmtpConfig
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool EnableSSL { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+}
+
